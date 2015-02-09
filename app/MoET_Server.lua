de26a62d55d329b8987b61ce5cf033a3fd313b75
joseoyola <joseoyola@gmail.com>
2015-02-08 22:25:29 -0800

    Changed backlight color, reading temp sense data

--- app/MoET_Server.lua
+++ app/MoET_Server.lua
@@ -1,5 +1,6 @@
 require "cord" -- scheduler / fiber library
 LCD = require("lcd")
+TEMP = require("temp")
 
 print("echo test")
 
@@ -16,10 +17,16 @@ print("node id", storm.os.nodeid())
 cport = 49352
 successes = 0
 iterations = 0
+tempValue = 0
 
 function lcd_setup()
 	lcd = LCD:new(storm.i2c.EXT, 0x7c, storm.i2c.EXT, 0xc4)
 	cord.new(function() lcd:init(2,1) lcd:setBackColor(20,20,20) end)
+end
+
+function temp_setup()
+	temp = TEMP:new()
+	cord.new(function() temp:init() end)
 end
 
 function stringStarts(String,Start)
@@ -34,6 +41,7 @@ end
 --create echo server as handler
 server = function()
    lcd_setup()
+   temp_setup()
    ssock = storm.net.udpsocket(7,serverHandler)
    print("Server started")
 end
@@ -44,27 +52,30 @@ serverHandler = function(payload, from, port)
 		if payload and stringStarts(payload,"1") then
 			successes = successes + 1
 			local count = string.sub(payload,3,string.len(payload))
-			print("Got 1 Data")
+			print("Received Data 1")
 			--cord.new(function() lcd:setBackColor(200,50,50) end)
 			--cord.new(function() lcd:clear() end)
 			--cord.new(function() lcd:writeString(string.format("Network: %d of %s packets received",successes,count)) end)
-			if iterations >= 10 then 
-				cord.new(function() lcd:writeString(string.sub("Network: " .. successes*100/count .. "." .. successes*1000/count % 10 .. "%" .. " received                     ",1,32)) end)
+			if iterations >= 5 then 
+				cord.new(function() lcd:setBackColor(20,100,20) lcd:writeString(string.sub("Network:        " .. successes*100/count .. "." .. successes*1000/count % 10 .. "%" .. " received                     ",1,32)) end)
 				iterations = 0
 			end
 		elseif payload and stringStarts(payload,"2") then
 			successes = 0
 			local value = string.sub(payload,3,string.len(payload))
-			if iterations >= 10 then 
-				cord.new(function() lcd:writeString(string.sub("Acc XYZ:        " .. value .. "                                         ",1,32)) end)
+			print("Received Data 2")
+			if iterations >= 5 then 
+				cord.new(function() lcd:setBackColor(20,20,100) lcd:writeString(string.sub("Acc XYZ:        " .. value .. "                                         ",1,32)) end)
 				iterations = 0
 			end
 			
 		elseif payload and stringStarts(payload,"3") then
 			successes = 0
-			local value = string.sub(payload,3,string.len(payload))
-			if iterations >= 10 then 
-				cord.new(function() lcd:writeString(string.sub("Temperature: " .. value .. "°C                                         ",1,32)) end)
+			--local value = string.sub(payload,3,string.len(payload))
+			cord.new(function() tempValue = temp:getTemp() end)
+			print("Received Data 3")
+			if iterations >= 5 then 
+				cord.new(function() lcd:setBackColor(100,20,20) lcd:writeString(string.sub("Temperature:    " .. tempValue .. string.char(0xdf) ..  "C                                         ",1,32)) end)
 				iterations = 0
 			end
 			
